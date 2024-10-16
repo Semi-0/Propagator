c6d3a499ca64a7dba3b705dca675e96e057e4371
semi <gtlpd21@gmail.com>
2024-10-16 21:15:54 +0200

    added divide and subtract constraint propagators

--- BuiltInProps.ts
+++ BuiltInProps.ts
@@ -11,13 +11,25 @@ import { get_support_layer_value } from "sando-layer/Specified/SupportLayer";
 import { pipe } from "fp-ts/lib/function";
 import { merge, tap, type Reactor } from "./Reactivity/Reactor";
 import { map } from "./Reactivity/Reactor";
+import { add, subtract} from "./Cell/GenericArith";
+
+export const p_add =  primitive_propagator((...inputs: any[]) => {
+    const result = inputs.reduce((acc, curr) => add(acc, curr), 0);
+    return result;
+}, "add");
+
+export const p_subtract =  primitive_propagator((...inputs: any[]) => {
+    const result = inputs.slice(1).reduce((acc, curr) => subtract(acc, curr), inputs[0]);
+    return result;
+}, "subtract");
+
 export const p_multiply =  primitive_propagator((...inputs: any[]) => {
     const result = inputs.slice(1).reduce((acc, curr) => multiply(acc, curr), inputs[0]);
    
     return result;
 }, "multiply");
 
-export const p_subdivide = primitive_propagator((...inputs: any[]) => {
+export const p_divide = primitive_propagator((...inputs: any[]) => {
     // console.log("subdivide inputs", inputs);
     return inputs.slice(1).reduce((acc, curr) => divide(acc, curr), inputs[0]);
 }, "subdivide"); 
@@ -26,12 +38,44 @@ export const p_subdivide = primitive_propagator((...inputs: any[]) => {
 export function c_multiply(x: Cell, y: Cell, product: Cell){
     return constraint_propagator([x, y, product], () => {
         const m = p_multiply(x, y, product).getActivator();
-        const s1 = p_subdivide(product, x, y).getActivator();
-        const s2 = p_subdivide(product, y, x).getActivator();
+        const s1 = p_divide(product, x, y).getActivator();
+        const s2 = p_divide(product, y, x).getActivator();
 
         return merge(m, s1, s2) as Reactor<any>
     }, "c:*")
 }
+
+export function c_subtract(x: Cell, y: Cell, difference: Cell){
+    return constraint_propagator([x, y, difference], () => {
+        const m = p_subtract(x, y, difference).getActivator();
+        const s1 = p_divide(difference, x, y).getActivator();
+        const s2 = p_divide(difference, y, x).getActivator();
+
+        return merge(m, s1, s2) as Reactor<any>
+    }, "c:-")
+}
+
+export function c_divide(x: Cell, y: Cell, quotient: Cell){
+    return constraint_propagator([x, y, quotient], () => {
+        const m = p_divide(x, y, quotient).getActivator();
+        const s1 = p_divide(quotient, x, y).getActivator();
+        const s2 = p_divide(quotient, y, x).getActivator();
+
+        return merge(m, s1, s2) as Reactor<any>
+    }, "c:/")
+}   
+
+export function c_add(x: Cell, y: Cell, sum: Cell){
+    return constraint_propagator([x, y, sum], () => {
+        const m = p_add(x, y, sum).getActivator();
+        const s1 = p_subtract(sum, x, y).getActivator();
+        const s2 = p_subtract(sum, y, x).getActivator();
+
+        return merge(m, s1, s2) as Reactor<any>
+    }, "c:+")
+}
+
+
 
 function construct_amb_reactor(f: () => void): () => Reactor<boolean>{
   return () =>  tap<boolean>(f)(observe_premises_has_changed() as Reactor<boolean>)
