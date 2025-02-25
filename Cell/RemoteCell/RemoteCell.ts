5b38f0698551759cf295afcd63e5eda59ddf5427
PRim <longpp17@163.com>
2025-02-25 22:38:04 +0100

    Revert "abstracted"
    This reverts commit d2d088dc0dac308435d97ce3140238eea54cc02b.

--- Cell/RemoteCell/RemoteCell.ts
+++ Cell/RemoteCell/RemoteCell.ts
@@ -1,22 +1,28 @@
-
+import type { Socket } from "bun";
 import { make_relation } from "../../DataTypes/Relation";
-import { get_global_parent, PublicStateCommand, set_global_state } from "../../Shared/PublicState";
+import { deep_equal, get_global_parent, PublicStateCommand, set_global_state } from "../../Shared/PublicState";
 import { scheduled_reactive_state } from "../../Shared/Reactivity/Scheduler";
 import { the_contradiction, the_nothing } from "../CellValue";
-
+import { pipe } from "fp-ts/lib/function";
 import { general_contradiction, type Cell } from "../Cell";
-
+import { strongest_value } from "../StrongestValue";
+import { construct_reactor, subscribe } from "../../Shared/Reactivity/Reactor";
+import { map, filter } from "../../Shared/Reactivity/Reactor";
 import { handle_contradiction } from "../Cell";
-
+import { option, strong } from "fp-ts";
+import { cell_merge } from "../Merge";
 import type { Propagator } from "../../Propagator/Propagator";
 import { describe } from "../../Helper/UI";
+import { to_string } from "generic-handler/built_in_generics/generic_conversation";
 import { mark_error } from "sando-layer/Specified/ErrorLayer"
 import SuperJSON from "superjson";
-
+import { is_layered_object } from "../../Helper/Predicate";
+import type { Option } from "fp-ts/lib/Option";
+import { none, some } from "fp-ts/lib/Option";
 import { construct_simple_generic_procedure } from "generic-handler/GenericProcedure";
 import type { RemoteConnector } from "../../RemoteServer/RemoteConnector";
 import * as E from "fp-ts/lib/Either"
-
+import { left, right } from "fp-ts/lib/Either";
 
 
 
