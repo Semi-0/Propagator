17254713456c4ce81cc3b4cd73b41c217e66b86f
PRim <longpp17@163.com>
2025-04-07 23:54:03 +0200

    added the transform_to_legit_propagator_function

--- AdvanceReactivity/Generics/GenericArith.ts
+++ AdvanceReactivity/Generics/GenericArith.ts
@@ -1,7 +1,7 @@
 // import { construct_simple_generic_procedure, define_generic_procedure_handler } from "generic-handler/GenericProcedure";
 // import { ValueSet } from "./ValueSet";
 // import { match_args } from "generic-handler/Predicates";
-import { add as _add, subtract as _subtract, multiply as _multiply, divide as _divide, less_than as _less_than,  greater_than as _greater_than, is_equal as _is_equal } from "generic-handler/built_in_generics/generic_arithmetic";
+import { add as _add, subtract as _subtract, multiply as _multiply, divide as _divide, less_than as _less_than,  greater_than as _greater_than } from "generic-handler/built_in_generics/generic_arithmetic";
 import { base_equal as _equal } from "../../Shared/base_equal";
 import { all_match, match_args, one_of_args_match, register_predicate } from "generic-handler/Predicates";
 import { get_base_value, is_contradiction, is_nothing, the_contradiction, the_nothing } from "../../Cell/CellValue";
@@ -17,21 +17,13 @@ const _and = construct_simple_generic_procedure("and", 2, (a: any, b: any) => a 
 const _or = construct_simple_generic_procedure("or", 2, (a: any, b: any) => a || b)
 const _not = construct_simple_generic_procedure("not", 1, (x: any) => !x)
 
-
 export const define_nothing_arithmetic_handler = (op: any) => define_generic_procedure_handler(op, 
     one_of_args_match(is_nothing), 
     (a: any, b: any) => {
             return the_nothing
     })
 
-define_nothing_arithmetic_handler(_add)
-define_nothing_arithmetic_handler(_subtract)
-define_nothing_arithmetic_handler(_multiply)
-define_nothing_arithmetic_handler(_divide)
-define_nothing_arithmetic_handler(_less_than)
-define_nothing_arithmetic_handler(_equal)
-define_nothing_arithmetic_handler(_and)
-define_nothing_arithmetic_handler(_or)
+
 
 export const define_contradiction_arithmetic_handler = (op: any) => define_generic_procedure_handler(op, 
     one_of_args_match(is_contradiction),
@@ -39,81 +31,43 @@ export const define_contradiction_arithmetic_handler = (op: any) => define_gener
         return the_contradiction
     }
 )
-define_contradiction_arithmetic_handler(_add)
-define_contradiction_arithmetic_handler(_subtract)
-define_contradiction_arithmetic_handler(_multiply)
-define_contradiction_arithmetic_handler(_divide)
-define_contradiction_arithmetic_handler(_less_than)
-define_contradiction_arithmetic_handler(_equal)
-define_contradiction_arithmetic_handler(_and)
-define_contradiction_arithmetic_handler(_or)
 
 
-export const layered_add = make_layered_procedure("layered_add", 2, (x: any, y: any) =>{  return _add(x, y)})
-export const layered_subtract = make_layered_procedure("layered_subtract", 2, (x: any, y: any) => _subtract(x, y))
-export const layered_multiply = make_layered_procedure("layered_multiply", 2, (x: any, y: any) =>{ return _multiply(x, y)})
-export const layered_divide = make_layered_procedure("layered_divide", 2, (x: any, y: any) => _divide(x, y))
-export const layered_not = make_layered_procedure("layered_not", 1, (x: any) => !x)
-export const layered_less_than = make_layered_procedure("layered_less_than", 2, (x: any, y: any) => x < y)
-export const layered_equal = make_layered_procedure("layered_equal", 2, (x: any, y: any) => x === y)
+export const define_layered_procedure_handler = (name: string, arity: number, op: any) => {
+    const layered_op = make_layered_procedure(name, arity, op)
+    define_generic_procedure_handler(op,
+        all_match(is_layered_object),
+        (a: any, b: any) => {
+            return layered_op(a, b)
+        }
+    )
+}
 
-define_generic_procedure_handler(_add,
-    all_match(is_layered_object),
-    (a: any, b: any) => {
-        return layered_add(a, b)
-    }
-)
+export const extend_propagator_arithmetic_pack = (name: string, arity: number, op: (a: any, b: any) => any) => {
+   define_nothing_arithmetic_handler(op)
+   define_contradiction_arithmetic_handler(op)
+   define_layered_procedure_handler(name, arity, op)
+   return op
+}
 
-define_generic_procedure_handler(_subtract,
-    all_match(is_layered_object),
-    (a: any, b: any) => {
-        return layered_subtract(a, b)
-    }
-)
+export const transform_to_legit_propagator_function = (name: string, arity: number, op: any) => {
+    const generic_op = construct_simple_generic_procedure(name, arity, op)
+    extend_propagator_arithmetic_pack(name, arity, generic_op)
+    return generic_op
+}
+
+export const add = extend_propagator_arithmetic_pack("add", 2, _add)
+export const subtract = extend_propagator_arithmetic_pack("subtract", 2, _subtract)
+export const multiply = extend_propagator_arithmetic_pack("multiply", 2, _multiply)
+export const divide = extend_propagator_arithmetic_pack("divide", 2, _divide)
+export const not = extend_propagator_arithmetic_pack("not", 1, _not)
+export const less_than = extend_propagator_arithmetic_pack("less_than", 2, _less_than)
+export const equal = extend_propagator_arithmetic_pack("equal", 2, _equal)
+export const and = extend_propagator_arithmetic_pack("and", 2, _and)
+export const or = extend_propagator_arithmetic_pack("or", 2, _or)
+export const greater_than = extend_propagator_arithmetic_pack("greater_than", 2, _greater_than)
 
 
-define_generic_procedure_handler(_multiply,
-    all_match(is_layered_object),
-    (a: any, b: any) => {
-        return layered_multiply(a, b)
-    }
-)
-
-define_generic_procedure_handler(_divide,
-    all_match(is_layered_object),
-    (a: any, b: any) => {
-        return layered_divide(a, b)
-    }
-)
-
-define_generic_procedure_handler(_not,
-    all_match(is_layered_object),
-    (a: any) => {
-        return layered_not(a)
-    }
-)
-
-define_generic_procedure_handler(_less_than,
-    all_match(is_layered_object),
-    (a: any, b: any) => {
-        return layered_less_than(a, b)
-    }
-)
-
-define_generic_procedure_handler(_equal,
-    all_match(is_layered_object),
-    (a: any, b: any) => {
-        return layered_equal(a, b)
-    }
-)
-
-export const add = _add
-export const subtract = _subtract
-export const multiply = _multiply
-export const divide = _divide
-export const not = _not
-export const less_than = _less_than
-export const equal = _equal
 
 export const force_load_arithmetic = () => {
     
